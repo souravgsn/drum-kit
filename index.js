3f82912d873b5fe6f8855256f9142ff2290c43f2
souravgsn <souravgsn@gmail.com>
2023-03-07 22:54:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+var num = document.querySelectorAll(".drum");
+
+for (var i = 0; i < num.length; i++) {
+    num[i].addEventListener("click", function () {
+        var x = this.innerHTML;
+
+		makeSound(x);
+    });
+}
+
+document.addEventListener("keypress",function(event){
+	makeSound(event.key);
+});
+
+
+
+
+function makeSound(key) {
+    switch (key) {
+        case "a":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "i":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+    }
+}
